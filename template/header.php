5c2256862ad8664b5e4ee17fd047530038b7c846
Ludwig Jedde <ludwigjedde.pro@gmail.com>
2018-06-05 08:26:28 +0200

    changé niveau du H

--- template/header.php
+++ template/header.php
@@ -4,7 +4,7 @@
         <div class="media-body">
             <img src="./public/images/avatar.png" alt="Jean Forteroche" class="mr-3 mt-3 rounded-circle" style="width:300px;">
             <br><br>
-            <h4>Jean Forteroche <br><small><i>le 26 mai 2018</i></small></h4>
+            <h1 class="home-title">Jean Forteroche <br><small><i>le 26 mai 2018</i></small></h1>
             <blockquote>"C'est avec un grand enthousiasme que je vous présente mon nouveau livre"</blockquote>
         </div>
     </div>
