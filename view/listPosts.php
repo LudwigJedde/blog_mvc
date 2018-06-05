e5b2ee9790088ab31eb0a822543ab383c56cb785
Ludwig Jedde <ludwigjedde.pro@gmail.com>
2018-06-05 08:27:37 +0200

    changé lien style css

--- view/listPosts.php
+++ view/listPosts.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css?family=Arvo|Dancing+Script" rel="stylesheet">
     <link rel="stylesheet" href="public/nav.css">
     <link rel="stylesheet" href="public/margin.css">
-    <link rel="stylesheet" href="public/style.css">
+    <link rel="stylesheet" href="public/styleAdmin.css">
     <title>Gestion des chapitres</title>
 </head>
 <body>
