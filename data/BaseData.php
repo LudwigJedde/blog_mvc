f9c4e6183a198a034f5017692827c67a75b7dcf7
Ludwig Jedde <ludwigjedde.pro@gmail.com>
2018-06-05 08:22:39 +0200

    supprimé de l'arbo dossier Data

--- data/BaseData.php
+++
@@ -1,9 +0,0 @@
-<?php
-class BaseData
-{
-    protected function dbConnect()
-    {
-        $db = new PDO('mysql:host=localhost;dbname=projet_4;charset=utf8', 'root', '');
-        return $db;
-    }
-}
